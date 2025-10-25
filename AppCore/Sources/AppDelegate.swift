ebaad7e96484758c8c5135a58fcb8376bfa72568
tylerlantern <tylerlantern@gmail.com>
2025-10-25 15:59:41 +0700

    add delege

--- AppCore/Sources/AppDelegate.swift
+++ AppCore/Sources/AppDelegate.swift
@@ -1,8 +1,36 @@
-//
-//  AppDelegate.swift
-//  AppCore
-//
-//  Created by Nattapong Unaregul on 25/10/2568 BE.
-//
-
+import UIKit
 import Foundation
+import AuthProvidersClient
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+	
+	var authProvidersClient: AuthProvidersClient?
+	
+	func application(
+		_ application: UIApplication,
+		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+	) -> Bool {
+		
+		return true
+	}
+	
+//	func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
+//		
+//	}
+	
+//	func application(_ app: UIApplication,
+//									 open url: URL,
+//									 options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+//		var handled: Bool
+//
+//		handled = GIDSignIn.sharedInstance.handle(url)
+//		if handled {
+//			return true
+//		}
+//
+//		// Handle other custom URL schemes if needed
+//
+//		return false
+//	}
+	
+}
